0fe08f085af356cfbaf7c17e55fd6ac0f9792d9b
聂述龙 <627972381@qq.com>
2019-04-24 23:00:38 +0800

    update playerinput

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput
+{
+    float velocity = 0;
+    float acceleration = 0;
+    float brake = 0;
+    float turn = 0;
+    bool jump = false;
+
+}
