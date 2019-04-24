a4e0c319f4509626ea7b55d5dacfa496debd2088
聂述龙 <627972381@qq.com>
2019-04-24 23:22:42 +0800

    update MM calc cost factor

---
+++ Assets/Scripts/MotionCostFactorSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MotionCostFactorSettings", menuName = "MotionMatcher/MotionCostFactorSettings")]
+public class MotionCostFactorSettings : ScriptableObject
+{
+    [Tooltip("")]
+    public float bonePosFactor = 1.0f;
+
+    [Tooltip("")]
+    public float boneRotFactor = 1.0f;
+
+    [Tooltip("")]
+    public float boneVelFactor = 1.0f;
+
+    [Tooltip("")]
+    public float rootMotionVelFactor = 1.5f;
+
+    [Tooltip("")]
+    public float predictionTrajectoryPosFactor = 2.0f;
+
+    [Tooltip("")]
+    public float predictionTrajectoryRotFactor = 2.0f;
+}
